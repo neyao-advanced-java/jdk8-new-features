40b25c4bb93921c986db3c0adcd964f25adaf9b2
姚能俊 <ynj@deepdata.com.cn>
2017-02-23 17:08:42 +0800

    first commit

---
+++ src/main/java/org/oursight/learning/java/App.java
@@ -0,0 +1,13 @@
+package org.oursight.learning.java;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
