8a2c0a7123e071c7c7ecc0eadbf44a7aa72b2a71
姚能俊 <ynj@deepdata.com.cn>
2017-03-07 14:18:17 +0800

    add stream api example

---
+++ src/main/java/org/oursight/learning/jdk18/stream/StreamApiBasicExample.java
@@ -0,0 +1,24 @@
+package org.oursight.learning.jdk18.stream;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Stream;
+
+/**
+ * Created by neyao on 2017/3/7.
+ */
+public class StreamApiBasicExample {
+
+    public static void main(String[] args) {
+        List<Integer> numbers = Arrays.asList(1, 2, 3, 4, 5, 6, 7, 8, 9, 10);
+
+        Stream<Integer> stream = numbers.stream();
+        stream.filter(
+                (x -> {
+                    return x % 2 == 0;
+                })
+        ).forEach(System.out::println);
+    }
+
+
+}
